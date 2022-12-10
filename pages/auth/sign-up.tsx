34075bd6d271210bd69abb8e752977d3f4481ab8
Anggairham <anggairham08@gmail.com>
2022-12-10 15:51:05 +0700

    slicing sign up

--- pages/auth/sign-up.tsx
+++ pages/auth/sign-up.tsx
@@ -7,18 +7,30 @@ import TextInput from "../../components/TextInput";
 import * as Yup from "yup";
 
 const SigninSchema = Yup.object().shape({
+  fullname: Yup.string()
+    .min(8, "Full name should have at least 8 characters")
+    .max(30, "Full name should have maximun 30 characters")
+    .required("Full name is required"),
   email: Yup.string()
     .email("Email format is invalid")
     .required("Email is required"),
-  password: Yup.string().required("Password is required"),
+  password: Yup.string()
+    .min(8, "Password should have at least 8 characters")
+    .max(50, "Password should have maximun 30 characters")
+    .required("Password is required"),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Confirm Password is mismatch")
+    .required("Confirm Password is required"),
 });
 type Props = {};
 
-const SignInPage = (props: Props) => {
+const SignUpPage = (props: Props) => {
   const formik = useFormik({
     initialValues: {
+      fullname: "",
       email: "",
       password: "",
+      confirmPassword: "",
     },
     validationSchema: SigninSchema,
     onSubmit: () => {
@@ -28,16 +40,27 @@ const SignInPage = (props: Props) => {
   return (
     <div>
       <Head>
-        <title>Sign In | Noobium</title>
+        <title>Sign Up | Noobium</title>
       </Head>
       <Navbar />
       <div className='w-[400px] mx-auto py-24'>
         <h1 className='font-sans font-bold text-slate-900 text-5xl text-center mb-4'>
-          Sign In
+          Sign Up
         </h1>
         <p className='font-sans text-slate-900 text-center mb-16'>
-          Fill the form to sign in to your account.
+          Fill the form to create an account.
         </p>
+        <TextInput
+          name='fullname'
+          label='Full Name'
+          type='text'
+          placeholder='Enter your full name'
+          value={formik.values.fullname}
+          onChange={formik.handleChange}
+          hasError={!!formik.errors.fullname}
+          errorMessage={formik.errors.fullname}
+        />
+        <div className='h-4'></div>
         <TextInput
           name='email'
           label='Email Address'
@@ -59,14 +82,25 @@ const SignInPage = (props: Props) => {
           hasError={!!formik.errors.password}
           errorMessage={formik.errors.password}
         />
+        <div className='h-4'></div>
+        <TextInput
+          name='confirmPassword'
+          label='Confirm Password'
+          type='password'
+          placeholder='Enter your confirm password'
+          value={formik.values.confirmPassword}
+          onChange={formik.handleChange}
+          hasError={!!formik.errors.confirmPassword}
+          errorMessage={formik.errors.confirmPassword}
+        />
         <div className='h-10'></div>
         <Button size='large' isFullWidth onClick={() => formik.handleSubmit()}>
-          Sign in
+          Sign Up
         </Button>
         <p className='font-sans text-slate-900 text-sm text-center mt-8'>
-          Don&lsquo;t have an account ?
-          <Link href='/auth/sign-up'>
-            <span className='text-blue-800'> Sign up here</span>
+          Already have an account ?
+          <Link href='/auth/sign-in'>
+            <span className='text-blue-800'>Sign in here</span>
           </Link>
         </p>
       </div>
@@ -74,4 +108,4 @@ const SignInPage = (props: Props) => {
   );
 };
 
-export default SignInPage;
+export default SignUpPage;

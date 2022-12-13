eae59b9a4cb7421f07f026c84304ff257dbb6248
Anggairham <anggairham08@gmail.com>
2022-12-13 20:48:25 +0700

    integrasi article detail page

---
+++ pages/api/articles/[slug].ts
@@ -0,0 +1,80 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type metaData = {
+  code: number;
+  status: string;
+  message: string;
+};
+type articleData = {
+  current_page: number;
+  data: {
+    id: number;
+    user_id: number;
+    category_id: number;
+    title: string;
+    slug: string;
+    content_preview: string;
+    content: string;
+    feature_image: string;
+    created_at: string;
+    updated_at: string;
+    category: {
+      id: number;
+      name: string;
+      slug: string;
+    };
+    user: {
+      id: number;
+      name: string;
+      email: string;
+      picture: string;
+    };
+  };
+};
+type Data = {
+  meta: metaData;
+  data: articleData;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({
+    meta: {
+      code: 200,
+      status: "sucess",
+      message: "Articles fetched successfully",
+    },
+    data: {
+      current_page: 1,
+      data: {
+        id: 1,
+        user_id: 1,
+        category_id: 1,
+        title:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptas dolorum illum",
+        slug: "lorem-ipsum",
+        content_preview:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sit porro sapiente non aut quaerat veniam expedita dolor, aliquam dolores impedit voluptates architecto, temporibus libero. Autem accusantium illum molestias magnam illo, adipisci deleniti vero eos magni suscipit neque cupiditate, veritatis veniam temporibus! Earum assumenda laborum eius excepturi sint recusandae quas.",
+        content:
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. At maxime dicta corrupti sunt nesciunt, consequuntur beatae aliquid earum ipsam, natus quae iste, porro reprehenderit. Nihil voluptatibus accusantium, natus hic aperiam eum aliquid, quidem in sequi dolore quia earum nam illo. Quasi, corrupti? Doloremque quas, expedita accusantium voluptates amet, aspernatur perferendis accusamus quisquam asperiores enim doloribus unde. At laudantium quas sint architecto, deserunt nulla inventore doloremque, repudiandae officiis similique, ratione molestias laboriosam illo quo autem delectus modi explicabo? Error blanditiis quas dolorum eum voluptatibus ipsum ab consequatur nobis laborum. Nesciunt recusandae ipsam veniam temporibus perspiciatis nostrum commodi, cumque est ipsum eaque. Lorem ipsum dolor, sit amet consectetur adipisicing elit. At maxime dicta corrupti sunt nesciunt, consequuntur beatae aliquid earum ipsam, natus quae iste, porro reprehenderit. Nihil voluptatibus accusantium, natus hic aperiam eum aliquid, quidem in sequi dolore quia earum nam illo. Quasi, corrupti? Doloremque quas, expedita accusantium voluptates amet, aspernatur perferendis accusamus quisquam asperiores enim doloribus unde. At laudantium quas sint architecto, deserunt nulla inventore doloremque, repudiandae officiis similique, ratione molestias laboriosam illo quo autem delectus modi explicabo? Error blanditiis quas dolorum eum voluptatibus ipsum ab consequatur nobis laborum. Nesciunt recusandae ipsam veniam temporibus perspiciatis nostrum commodi, cumque est ipsum eaque.",
+        feature_image: "/images/dummy-article-thumbnail.png",
+        created_at: "2022-09-20 18:00:00",
+        updated_at: "2022-09-20 18:00:00",
+        category: {
+          id: 1,
+          name: "Technology",
+          slug: "technology",
+        },
+        user: {
+          id: 1,
+          name: "John Doe",
+          email: "john@gmail.com",
+          picture: "/images/dummy-avatar.png",
+        },
+      },
+    },
+  });
+}
--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,22 +1,57 @@
 import { parseISO, format } from "date-fns";
+import { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Navbar from "../../components/Navbar";
-export default function ArticleDetailPage() {
-  const article = {
-    id: 1,
-    title: "Learnign Redugx",
-    content:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, unde velit asperiores magni corporis dicta aut ipsa eum officiis repellendus quos ipsam assumenda earum corrupti distinctio commodi. \n\nNeque accusantium minima amet laboriosam. Sapiente illum aspernatur expedita natus, consectetur maxime ipsa vel ea dolorum, iste veritatis ducimus ut odit a facere voluptate corporis excepturi! Reiciendis ipsa quis rerum, saepe mollitia fugiat error tempore voluptas necessitatibus animi fuga quod aspernatur corporis aut magni qui architecto ab? Excepturi totam dolorum facilis mollitia corrupti?",
-    url: `/article/how-to-learn-redux`,
-    thumbnail: "/images/dummy-article-thumbnail.png",
-    category: "Technology",
-    date: "2022-09-20 18:00:00",
-    author: {
-      name: "Joh Doe",
-      photo: "/images/dummy-avatar.png",
-    },
+import axios from "../../helpers/axios";
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  try {
+    const response = await axios.get(`/articles/detail/${query.slug}`);
+    return {
+      props: {
+        article: response.data.data,
+      },
+    };
+  } catch (error: any) {
+    if (error.response.status === 404) {
+      return {
+        notFound: true,
+      };
+    }
+
+    return {
+      props: {},
+    };
+  }
+};
+type Props = {
+  article: {
+    id: number;
+    user_id: number;
+    category_id: number;
+    title: string;
+    slug: string;
+    content_preview: string;
+    content: string;
+    feature_image: string;
+    created_at: string;
+    updated_at: string;
+    category: {
+      id: number;
+      name: string;
+      slug: string;
+    };
+    user: {
+      id: number;
+      name: string;
+      email: string;
+      picture: string;
+    };
   };
-  const formattedDate = format(parseISO(article.date), "MMM dd");
+};
+const ArticleDetailPage: NextPage<Props> = ({ article }) => {
+  // const ArticleDetailPage = (article: Props) => {
+  // const formattedDate = format(parseISO(article.created_at), "MMM dd");
+  const formattedDate = "asd";
   return (
     <div>
       <Head>
@@ -29,12 +64,12 @@ export default function ArticleDetailPage() {
         <div className='flex items-center mb-8'>
           <img
             className='w-12 h-12 rounded-full object-contain mr-4'
-            src={article.author.photo}
-            alt={article.author.name}
+            src={article.user.picture}
+            alt={article.user.name}
           />
           <div>
             <p className='text-sm font-sans text-slate-900 mb-1'>
-              {article.author.name}
+              {article.user.name}
             </p>
             <p className='text-sm font-sans text-slate-400'>{formattedDate}</p>
           </div>
@@ -43,11 +78,13 @@ export default function ArticleDetailPage() {
           {article.title}
         </h1>
         <div className='px-3 bg-slate-200 rounded-full w-fit h-6 flex items-center mb-12'>
-          <p className='text-slate-900 font-sans text-xs'>{article.category}</p>
+          <p className='text-slate-900 font-sans text-xs'>
+            {article.category.name}
+          </p>
         </div>
         <img
           className='w-full aspect-video object-cover mb-12'
-          src={article.thumbnail}
+          src={article.feature_image}
           alt={article.title}
         />
         <p className='font-serif text-slate-900 whitespace-pre-line'>
@@ -56,4 +93,6 @@ export default function ArticleDetailPage() {
       </div>
     </div>
   );
-}
+};
+
+export default ArticleDetailPage;
